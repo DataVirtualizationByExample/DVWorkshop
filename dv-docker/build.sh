c71c1d7e8b59cecbaac13d2dcbcaa85d4ba82718
Co van Ballegooijen <cvanball@redhat.com>
2014-05-06 21:17:13 +0200

    Script to build JBoss Data Virtualization docker container, add check if dv software exists

--- dv-docker/build.sh
+++ dv-docker/build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-
-echo "building the JBoss Data Virtualization 6.0.0 container for workshop"
-docker build -t jbossdv600 .
+if [ -f "software/jboss-dv-installer-6.0.0.GA-redhat-4.jar" ]
+then
+        
+	echo "Building the JBoss Data Virtualization 6.0.0 container for workshop"
+	docker build -t jbossdv600 .
+else
+	echo "File software/jboss-dv-installer-6.0.0.GA-redhat-4.jar not found."
+        echo "Please download JBoss Data Virtualization 6.0.0.GA from http://jboss/products#IBP"
+        exit 0
+fi

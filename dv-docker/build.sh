4293185eb602ba9a82db98c0c0230e34b550d627
Co van Ballegooijen <cvanball@redhat.com>
2014-05-06 21:18:54 +0200

    Script to build JBoss Data Virtualization docker container

--- dv-docker/build.sh
+++ dv-docker/build.sh
@@ -6,6 +6,6 @@ then
 	docker build -t jbossdv600 .
 else
 	echo "File software/jboss-dv-installer-6.0.0.GA-redhat-4.jar not found."
-        echo "Please download JBoss Data Virtualization 6.0.0.GA from http://jboss/products#IBP"
+        echo "Please download JBoss Data Virtualization 6.0.0.GA from http://jboss.org/products#IBP"
         exit 0
 fi

9a74409c9a978bcf2b81131323197edd63648395
Cojan van Ballegooijen <cojan@redhat.com>
2015-11-21 13:57:05 +0100

    Converted to html/docbook to reflect changes in doc

--- webapp/project-netbeans/FinancialsWebApplication/build/web/resources/js/jsfcrud.js
+++
@@ -1,8 +0,0 @@
-function handleSubmit(args, dialog) {
-    var jqDialog = jQuery('#' + dialog);
-    if (args.validationFailed) {
-        jqDialog.effect('shake', {times: 3}, 100);
-    } else {
-        PF(dialog).hide();
-    }
-}
